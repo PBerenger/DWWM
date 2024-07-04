4858409140887ce28a5a91b66a9bf3a0d2a63c5d
PBerenger <b.pommelet@gmail.com>
2024-07-04 08:40:40 +0200

    JOmaj05/07/24

--- Projet Jeux olympique/template.php
+++ Projet Jeux olympique/template.php
@@ -12,38 +12,54 @@ if (session_status() == PHP_SESSION_NONE) session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Répertoire</title>
+    <title>jo_skateboard2024bady</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
-        <nav>
+        <nav class="connection">
+            <li><a href="create.php">S'inscrire</a></li>
+            <?php
+            if (isset($_SESSION["user_id"])) :
+            ?>
+                <li><a href="logout.php">Déconnection</a></li>
+            <?php else : ?>
+                <li><a href="login.php">Login</a></li>
+            <?php endif; ?>
+
+        </nav>
+        <nav class="navigation">
             <ul>
                 <li><a href="index.php">Accueil</a></li>
-                <li><a href="create.php">Ajouter un utilisateur</a></li>
-                <li><a href="read.php">Voir les utilisateurs</a></li>
+                <li><a href="read.php">Voir les utilisateurs (admin)</a></li>
                 <li><a href="update.php">Modifier/Supprimer un utilisateur</a></li>
-                <!-- <li><a href="delete.php">Supprimer un utilisateur</a></li> -->
-                <?php
-                if (isset($_SESSION["user_id"])) :
-                ?>
-                    <li><a href="logout.php">Déconnection</a></li>
-                <?php else :?>
-                    <li><a href="login.php">Login</a></li>
-                <?php endif;?>
-
-
+                <li><a href="delete.php">Supprimer un utilisateur</a></li>
             </ul>
 
         </nav>
     </header>
 
+
+
     <script src="js.js"></script>
 
+    <div class="content">
+        <h1><?= $titre ?></h1>
+        <?= $content ?>
+    </div>
 
-    <h1><?= $titre ?></h1>
-    <?= $content ?>
+    <footer>
+        <div class="foot1"></div>
+        <div class="foot2">
+            <div class="logoF"><img src="/img/all/Paris2024_Olywhiteleft.png" alt=""></div>
+            <ul>
+                <li>POMMELET Bérenger</li>
+                <li>Plan du site</li>
+                <li>Copyright</li>
+            </ul>
+        </div>
+    </footer>
 </body>
 
 </html>
