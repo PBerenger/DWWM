cb0488c727ab6c6495455d6b06ecc99bbae10eb3
PBerenger <b.pommelet@gmail.com>
2025-01-22 15:02:45 +0100

    majgo

--- 0. ProjetsFormation/ResaR/index.php
+++ 0. ProjetsFormation/ResaR/index.php
@@ -1,6 +1,12 @@
 <?php
-session_start();
-ob_start();
+//Empêcher le document d’expirer lorsque nous voulons revenir à une page avec une demande POST
+ini_set('session.cache_limiter', 'public');
+session_cache_limiter(false);
+//Démarrer la session
+if (session_status() !== PHP_SESSION_ACTIVE)
+    session_start();
+
+
 require_once './Controllers/router.php';
 $content = ob_get_clean();
-require_once __DIR__ . "/views/template.php";
+require_once "./views/template.php";
